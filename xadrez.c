cce8c66a27dc90ef7b9f52982061a00ea5b37844
Lucascec95 <Lucasbresende95@outlook.com>
2025-08-29 00:02:33 +0000

    https://probable-space-yodelg4wpxg6jr4w52v6g7.github.dev/

--- xadrez.c
+++ xadrez.c
@@ -3,15 +3,12 @@
 int main() {
  printf("população=6748000/Área: 1200.25")
 
- 
- float População: 6748000
-Área: 1200.25
+densidade_populacional = float(populacao) / area
+pib per capita = pib / float(populacao)
 
-
-
-
-
+print(f"\nNome da Cidade: {nome}")
+print(f"Densidade Populacional: {densidade_populacional:.2f} hab/km²")
+print(f"PIB per Capita: {pib per capita:.2f} reais")
 
  
-
 }
