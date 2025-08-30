6e72b8ec46aaf9e32f959b3a50273eefe465b566
Lucascec95 <Lucasbresende95@outlook.com>
2025-08-30 03:20:26 +0000

    https://automatic-space-lamp-x5q4rjx7prrvcpxgp.github.dev/

--- xadrez.c
+++ xadrez.c
@@ -10,5 +10,20 @@ print(f"\nNome da Cidade: {nome}")
 print(f"Densidade Populacional: {densidade_populacional:.2f} hab/km²")
 print(f"PIB per Capita: {pib per capita:.2f} reais")
 
- 
+
+print(f"População: Carta 1 venceu ({1 if carta1['populacao'] > carta2['populacao'] else 0})")
+
+print(f"Área: Carta 1 venceu ({1 if carta1['area'] > carta2['area'] else 0})")
+
+print(f"PIB: Carta 1 venceu ({1 if carta1['pib'] > carta2['pib'] else 0})")
+
+print(f"Pontos Turísticos: Carta 1 venceu ({1 if carta1['pontos_turisticos'] > carta2['pontos_turisticos'] else 0})")
+
+print(f"Densidade Populacional: Carta 1 venceu ({1 if carta1['densidade'] < carta2['densidade'] else 0})")
+
+print(f"PIB per Capita: Carta 1 venceu ({1 if carta1['pib_per_capita'] > carta2['pib_per_capita'] else 0})")
+
+print(f"Super Poder: Carta 1 venceu ({1 if carta1['super_poder'] > carta2['super_poder'] else 0})")
+
+
 }
