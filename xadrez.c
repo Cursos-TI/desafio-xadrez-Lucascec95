999c1687323815718d10cba2c7395938bb67fc53
Lucascec95 <Lucasbresende95@outlook.com>
2025-09-08 23:48:46 +0000

    mover peça xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 
-int main() {
- printf("população=6748000/Área: 1200.25")
+int main(){
 
-densidade_populacional = float(populacao) / area
-pib per capita = pib / float(populacao)
+// mover a torre 5 casas para a direita
+for (int i= 0; i < 5; i++) {
+  printf("frente\n"); // imprime a direção do movimento
+}
+// mover o bispo 5 casas para a diagonal a direita
+for (int i= 0; i < 5; i++) {
+  printf("direita\n"); // imprime a direção do movimento
+}
+// mover a rainha 8 casas para a esquerda
+for (int i= 0; i < 5; i++) {
+  printf("esquerda\n"); // imprime a direção do movimento
+}
 
-print(f"\nNome da Cidade: {nome}")
-print(f"Densidade Populacional: {densidade_populacional:.2f} hab/km²")
-print(f"PIB per Capita: {pib per capita:.2f} reais")
 
-
-print(f"População: Carta 1 venceu ({1 if carta1['populacao'] > carta2['populacao'] else 0})")
-
-print(f"Área: Carta 1 venceu ({1 if carta1['area'] > carta2['area'] else 0})")
-
-print(f"PIB: Carta 1 venceu ({1 if carta1['pib'] > carta2['pib'] else 0})")
-
-print(f"Pontos Turísticos: Carta 1 venceu ({1 if carta1['pontos_turisticos'] > carta2['pontos_turisticos'] else 0})")
-
-print(f"Densidade Populacional: Carta 1 venceu ({1 if carta1['densidade'] < carta2['densidade'] else 0})")
-
-print(f"PIB per Capita: Carta 1 venceu ({1 if carta1['pib_per_capita'] > carta2['pib_per_capita'] else 0})")
-
-print(f"Super Poder: Carta 1 venceu ({1 if carta1['super_poder'] > carta2['super_poder'] else 0})")
-
+return 0;
 
 }
